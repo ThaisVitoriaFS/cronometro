9d43327b11dcad5b51fe6d2f3ff3df48ef1b4571
Thais Vitoria <160156403+ThaisVitoriaFS@users.noreply.github.com>
2024-07-23 19:43:21 -0300

    Add files via upload

--- cronometro/src/javascript/script.js
+++ cronometro/src/javascript/script.js
@@ -1,1 +1,15 @@
-alert ('alo')
+const timerEl = document.getElementById('timer');
+const marksList = document.getElementById('marks-list');
+let intervalid = 0;
+let timer = 0;
+let marks = [];
+
+const formatTime = (time) => {
+    const hours = Math.floor(time / 360000);
+    const minutes = Math.floor(time / 360000) / 6000;
+    const seconds = Math.floor(time / 6000) / 100;
+    const hundredths = time % 100;
+
+    /*return `$(hours.toString().padStart(2, '0')):`*/
+    return  $(hours.toString().padStart(2, '0'))
+}
