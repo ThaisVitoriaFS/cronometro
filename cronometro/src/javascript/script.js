b5c7494c93351148003e3be2927bda6891d40831
Thais Vitoria <160156403+ThaisVitoriaFS@users.noreply.github.com>
2024-07-23 23:09:38 -0300

    Add files via upload

--- cronometro/src/javascript/script.js
+++ cronometro/src/javascript/script.js
@@ -1,15 +1,63 @@
 const timerEl = document.getElementById('timer');
 const marksList = document.getElementById('marks-list');
-let intervalid = 0;
+let intervalId = 0;
 let timer = 0;
 let marks = [];
 
+/* formata o tempo*/
 const formatTime = (time) => {
     const hours = Math.floor(time / 360000);
-    const minutes = Math.floor(time / 360000) / 6000;
-    const seconds = Math.floor(time / 6000) / 100;
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
     const hundredths = time % 100;
 
-    /*return `$(hours.toString().padStart(2, '0')):`*/
-    return  $(hours.toString().padStart(2, '0'))
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${hundredths.toString().padStart(2, '0')}`;
 }
+
+const addMarksToList = (markIndex, markTime) => {
+    marksList.innerHTML += `<p>Marca ${markIndex}: ${formatTime(markTime)}</p>`;
+}
+
+const markTime = () => {
+    marks.push(timer);
+    addMarksToList(marks.length, timer);
+}
+
+/*faz o tempo acontecer*/
+const toggleTimer = () => {
+    const button = document.getElementById('power');
+    const action = button.getAttribute('action');
+
+    clearInterval(intervalId);
+
+    if (action == 'start' || action == 'continue') {
+        intervalId = setInterval(() => {
+            timer += 1;
+        }, 10);
+        button.setAttribute('action', 'pause');
+        /*trocar o botão de pausa*/
+        button.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    } else if (action == 'pause') {
+        button.setAttribute('action', 'continue');
+        button.innerHTML = '<i class="fa-solid fa-play"></i>';
+    }
+}
+
+const resetTimer = () => {
+    clearInterval(intervalId);
+    timer = 0;
+    marks [];
+    setTimer(timer);
+    marksList.innerHTML = '';
+    const button = document.getElementById('power');
+    button.setAttribute('action', 'start');
+    button.innerHTML = '<i class="fa-solid fa-play"></i>';
+}
+
+const setTimer = (time) => {
+    timerEl.innerText = formatTime(time);
+}
+
+document.getElementById('power').addEventListener('click', toggleTimer);
+document.getElementById('mark').addEventListener('click', markTime);
+document.getElementById('reset').addEventListener('click', resetTimer);
